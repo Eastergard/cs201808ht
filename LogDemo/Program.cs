fc5d0bfa0017283ee633b168e1a32bf9c443965b
Bjarke Ø. Nielsen <b.oest.nielsen@gmail.com>
2018-08-24 15:49:05 +0200

    Logging.

---
+++ LogDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogDemo
+{
+    class Program
+    {
+        private static NLog.Logger log = NLog.LogManager.GetCurrentClassLogger();
+
+
+        static void Main(string[] args)
+        {
+            log.Debug("App startet");
+
+            try
+            {
+                string g = null;
+                g.ToString();
+            }
+            catch (Exception e)
+            {
+                log.Error(e);
+            }
+        }
+    }
+}
