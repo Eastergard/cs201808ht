92ba0a7be0ad12ed109fcbb227dc78b746a8ba91
Bjarke Ø. Nielsen <b.oest.nielsen@gmail.com>
2018-08-24 11:01:06 +0200

    Enum og Struct.

---
+++ VariablerOpg3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablerOpg3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Udskrver csc og 0.
+            Filtyper ft = Filtyper.csv;
+            Console.WriteLine(ft);
+            Console.WriteLine((int)ft);
+
+            // Danner en person "Mikkel" med id 1.
+            Person p = new Person()
+            {
+                Navn = "Mikkel",
+                Id = 1
+            };
+            Console.WriteLine(p.Navn);
+            Console.WriteLine(p.Id);
+
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// Person
+    /// </summary>
+    struct Person
+    {
+        public int Id;
+        public string Navn;
+    }
+
+    /// <summary>
+    /// Filtyper
+    /// </summary>
+    enum Filtyper
+    {
+        csv,
+        pdf,
+        txt
+    }
+
+}
