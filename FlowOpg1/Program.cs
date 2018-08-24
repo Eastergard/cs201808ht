5e81915bae874601fccf9a9d62a37a2a6712e332
Bjarke Ø. Nielsen <b.oest.nielsen@gmail.com>
2018-08-24 12:23:02 +0200

    Multiplikationstabel.

---
+++ FlowOpg1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowOpg1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                int tal = 0;
+                for (int j = 1; j <= 10; j++)
+                {
+                    tal += i;
+                    if (tal > 50)
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    else
+                        Console.ForegroundColor = ConsoleColor.Gray;
+
+                    Console.Write(tal.ToString().PadLeft(4));
+                }
+
+                Console.Write("\n");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
