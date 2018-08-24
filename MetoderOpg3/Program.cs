2ab2850ac946e6fda7a157b898ef2569fe4f0162
Bjarke Ø. Nielsen <b.oest.nielsen@gmail.com>
2018-08-24 13:34:23 +0200

    Rekursion.

---
+++ MetoderOpg3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetoderOpg3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TælTil10();
+
+            Console.ReadKey();
+        }
+
+        static void TælTil10(int tal = 1)
+        {
+            if (tal > 10)
+                return;
+
+            Console.WriteLine(tal);
+            tal++;
+            TælTil10(tal);
+        }
+    }
+}
