0eba25d1e7cbc15d693b459fce1cb7708da40420
Bjarke Ø. Nielsen <b.oest.nielsen@gmail.com>
2018-08-24 13:20:24 +0200

    Små metoder.

---
+++ MetoderOpg1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetoderOpg1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = LægSammen(5, 2);
+            double res2 = BeregnAreal(5);
+
+            Console.WriteLine(res);
+            Console.WriteLine(res2);
+            Console.WriteLine("Dette er en test");
+
+            Console.ReadKey();
+        }
+
+        static int LægSammen(int a, int b) { return a + b; }
+
+        static double BeregnAreal(int radius)
+        {
+            double pi = Math.PI;
+
+            return radius * radius * pi;
+        }
+
+        static void Udskriv(string txt)
+        {
+            Console.WriteLine(txt);
+        }
+
+
+    }
+}
