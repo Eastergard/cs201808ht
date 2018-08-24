2faf7c92a39bf63d4d06beddb2b32b840fea5ad8
Bjarke Ø. Nielsen <b.oest.nielsen@gmail.com>
2018-08-24 13:26:01 +0200

    Overload.

---
+++ MetoderOpg2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetoderOpg2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Beregn(1, 2));
+            Console.WriteLine(Beregn(1, 2, 3));
+            Console.WriteLine(Beregn(1, 2, 4, 5));
+
+            Console.ReadKey();
+        }
+
+        static int Beregn(int a, int b) { return a + b; }
+
+        static int Beregn(int a, int b, int c) { return Beregn(Beregn(a, b), c); }
+
+        static int Beregn(int a, int b, int c, int d) { return Beregn(Beregn(a, b, c), d); }
+    }
+}
