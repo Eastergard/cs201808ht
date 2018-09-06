a1d6f39c6871106fd7426ea0942654181676081d
Bjarke Ø. Nielsen <b.oest.nielsen@gmail.com>
2018-09-06 15:45:44 +0200

    Split på string[].

---
+++ Arrays.Split/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays.Split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = "3;5;22;1;10;15;5";
+            string[] arr = txt.Split(';');
+
+            int sum = 0;
+            foreach (var tal in arr)
+            {
+                sum += Convert.ToInt32(tal);
+            }
+
+            Console.WriteLine(sum.ToString());
+            Console.ReadKey();
+        }
+    }
+}
