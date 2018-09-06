7ede84fb42b779eddcab1e4640e281905d6a430b
Bjarke Ø. Nielsen <b.oest.nielsen@gmail.com>
2018-09-06 15:37:57 +0200

    Print og gennemsnit af array.

---
+++ Arrays.Simpel/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays.Simpel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] månedsløn = { 10000, 20000, 15000, 20000, 30000, 15000 };
+
+            foreach (var løn in månedsløn)
+            {
+                Console.WriteLine(løn);
+            }
+
+            double gennemsnit = månedsløn.Average();
+
+            Console.WriteLine("Gennemsnit: " + gennemsnit.ToString("N2"));
+
+            Console.ReadKey();
+
+        }
+    }
+}
