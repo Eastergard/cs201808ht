ce2a124fb8ec6be8d8886e9271276cf505286201
Bjarke Ø. Nielsen <b.oest.nielsen@gmail.com>
2018-09-06 12:36:02 +0200

    Klasser med statiske metoder.

---
+++ Klasser.Statisk/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasser.Statisk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Beregninger b = new Beregninger(); fejl
+            int res = Beregninger.LægSammen(1, 1);
+            Console.WriteLine(res);
+
+            int res1 = Beregninger.TrækFra(1, 1);
+            Console.WriteLine(res1);
+
+
+            // Hold console åben ved debug
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    class Beregninger
+    {
+        private Beregninger() { }
+
+        public static int LægSammen(int a, int b) => a + b;
+
+        public static int TrækFra(int a, int b) => a - b;
+
+    }
+
+
+
+}
