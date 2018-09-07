94ae548dbea6f3c308770601443a64bbc1c64e6d
Bjarke Ø. Nielsen <b.oest.nielsen@gmail.com>
2018-09-07 14:20:14 +0200

    Polymorfi med dyr.

---
+++ Arv.Dyr.Polymorfi/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arv.DyrPolymorfi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dyr d = new Dyr();
+        }
+    }
+
+    protected class Dyr
+    {
+        private string _navn;
+
+        public string Navn
+        {
+            get { return _navn; }
+            set { _navn = value; }
+        }
+
+        public virtual void SigNoget()
+        {
+            Console.WriteLine("Jeg er et dyr og hedder " + this.Navn);
+        }
+
+    }
+
+    class Hund : Dyr
+    {
+        public override void SigNoget()
+        {
+            Console.WriteLine("Jeg er en hund og hedder "+ this.Navn);
+        }
+    }
+
+    class Kat : Dyr
+    {
+        public override void SigNoget()
+        {
+            Console.WriteLine("Jeg er en kat og hedder " + this.Navn);
+        }
+    }
+}
---
+++ Arv.DyrPolymorfi/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arv.DyrPolymorfi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dyr[] dyr = new Dyr[20];
+            for (int i = 0; i < 20; i++)
+            {
+                dyr[i] = Dyr.TilfældigtDyr();
+            }
+
+            foreach (var item in dyr)
+            {
+                item.SigNoget();
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Dyr
+    {
+
+        static Random _rnd = new Random();
+
+        public string Navn { get; set; }
+
+        public static Dyr TilfældigtDyr()
+        {
+            string sti = @"x:\dyrenavne.txt";
+            string[] navne = System.IO.File.ReadAllLines(sti);
+            int index = _rnd.Next(0, navne.Length);
+            if (index % 2 == 0)
+            {
+                return new Hund() { Navn = navne[index] };
+            }
+            else
+            {
+                return new Kat() { Navn = navne[index] };
+            }
+        }
+
+        public virtual void SigNoget()
+        {
+            Console.WriteLine("Jeg er et dyr og hedder " + this.Navn);
+        }
+
+    }
+
+    class Hund : Dyr
+    {
+        public override void SigNoget()
+        {
+            Console.WriteLine("Jeg er en hund og hedder " + this.Navn);
+        }
+    }
+
+    class Kat : Dyr
+    {
+        public override void SigNoget()
+        {
+            Console.WriteLine("Jeg er en kat og hedder " + this.Navn);
+        }
+    }
+}
