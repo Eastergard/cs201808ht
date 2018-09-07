bb8181bdc44ef4beed835a2b6a83235f4918a29a
Bjarke Ø. Nielsen <b.oest.nielsen@gmail.com>
2018-09-07 15:38:59 +0200

    Interface - IComparable

---
+++ Interfaces.Comparer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Comparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hund[] hunde = new Hund[2];
+            hunde[0] = new Hund() { Alder = 10, Navn = "Bulder" };
+            hunde[1] = new Hund() { Alder = 5, Navn = "Lady" };
+            Array.Sort(hunde);
+            foreach (var item in hunde)
+            {
+                Console.WriteLine(item.Navn);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Hund : IComparable<Hund>
+    {
+        public string Navn { get; set; }
+        public int Alder { get; set; }
+
+        public int CompareTo(Hund hund)
+        {
+            if (this.Alder > hund.Alder)
+                return 1;
+            if (this.Alder < hund.Alder)
+                return -1;
+            return 0;
+        }
+    }
+}
