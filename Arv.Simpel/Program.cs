055f15b015e9c2f402d9e22a2b900e73c3dd809f
Bjarke Ø. Nielsen <b.oest.nielsen@gmail.com>
2018-09-07 11:36:09 +0200

    Arv. Person->Elev og Person->Instrktør.

---
+++ Arv.Simpel/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arv.Simpel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person() { Fornavn = "Bjarke", Efternavn = "Nielsen" };
+            Console.WriteLine(p.FuldtNavn());
+
+            Elev e = new Elev() { Efternavn = "t", Fornavn = "g", Klasselokale = "A" };
+            Instruktør i = new Instruktør() { Efternavn = "2", Fornavn = "s", NøgleId = 1 };
+            Console.WriteLine(p.FuldtNavn());
+            Console.WriteLine(e.FuldtNavn());
+            Console.WriteLine(i.FuldtNavn());
+            // Hold console åben ved debug
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+
+
+    class Person
+    {
+        public string Fornavn { get; set; }
+        public string Efternavn { get; set; }
+
+        public Person()
+        {
+            this.Fornavn = "";
+            this.Efternavn = "";
+        }
+
+        public string FuldtNavn()
+        {
+            return this.Fornavn + " " + this.Efternavn;
+        }
+    }
+
+    class Elev : Person
+    {
+        public string Klasselokale { get; set; }
+    }
+
+    class Instruktør : Person
+    {
+        public int NøgleId { get; set; }
+    }
+}
