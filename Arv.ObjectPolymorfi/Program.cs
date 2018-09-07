78c410aea744d7696ac94c31378301215ab0ad5e
Bjarke Ø. Nielsen <b.oest.nielsen@gmail.com>
2018-09-07 14:26:37 +0200

    Polymorfi af ToString().

---
+++ Arv.ObjectPolymorfi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arv.ObjectPolymorfi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Faktura f = new Faktura() { Nr = 215, Dato = DateTime.Now.Date, Kunde = "Poul A. Andersen" };
+
+            Console.WriteLine(f.ToString());
+
+            Console.ReadKey();
+        }
+    }
+
+    class Faktura
+    {
+        public int Nr { get; set; }
+        public DateTime Dato { get; set; }
+        public string Kunde { get; set; }
+
+        public override string ToString()
+        {
+            return $"Faktura til {Kunde} nr {Nr} fra {Dato}";
+        }
+    }
+}
