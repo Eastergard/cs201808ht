cd8904d232ca58e41349f7be5585020d60c9d707
Bjarke Ø. Nielsen <b.oest.nielsen@gmail.com>
2018-09-07 10:13:24 +0200

    Samlinger: lister og dictionaries.

---
+++ Samlinger.Simpel/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samlinger.Simpel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var familie = new List<Person>();
+            familie.Add(new Person() { Id = 1, Navn = "Bjarke" });
+            familie.Add(new Person() { Id = 2, Navn = "Anja" });
+            familie.Add(new Person() { Id = 0, Navn = "Nano" });
+
+            foreach (var person in familie)
+            {
+                Console.WriteLine($"Person med ID: {person.Id} er {person.Navn}.");
+            }
+
+            var husstand = new Person().Husstand;
+            foreach (var person in familie)
+            {
+                husstand.Add(person.Id, person);
+            }
+
+            foreach (var person in husstand)
+            {
+                Console.WriteLine($"Person med ID: {person.Value.Id} er {person.Value.Navn}.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Person
+    {
+        public Person()
+        {
+            this.Husstand = new Dictionary<int, Person>();
+        }
+
+        public int Id { get; set; }
+        public string Navn { get; set; }
+
+        public Dictionary<int, Person> Husstand { get; set; }
+
+    }
+}
