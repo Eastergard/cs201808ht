c08513783e13e34cf511c2133fc88e564dc7401b
Bjarke Ø. Nielsen <b.oest.nielsen@gmail.com>
2018-09-07 11:42:35 +0200

    Udvidelse af System.Random klassen.

---
+++ Arv.Udvid/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arv.Udvid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdvidetRandom r = new UdvidetRandom();
+            Console.WriteLine(r.NextBool()); // true eller false
+
+            Console.ReadKey();
+        }
+    }
+
+
+    class UdvidetRandom : Random
+    {
+        public bool NextBool()
+        {
+            int tal = Next(1, 1002);
+            if (tal <= 500)
+                return true;
+            return false;
+        }
+    }
+}
