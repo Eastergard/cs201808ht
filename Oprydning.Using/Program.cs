8107dc8b90140153a7d68a36f3e42b747e7f955d
Bjarke Ø. Nielsen <b.oest.nielsen@gmail.com>
2018-09-20 15:28:19 +0200

    Oprydning vha. using.

---
+++ Oprydning.Using/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oprydning.Using
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader stream = System.IO.File.OpenText(@"X:\dyrenavne.txt"))
+            {
+                while (stream.Peek() != -1)
+                {
+                    string navn = stream.ReadLine();
+                    Console.WriteLine(navn);
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
