c0ff9784ddee0711b8ddd37e19eb08bdca3ac407
Bjarke Ø. Nielsen <b.oest.nielsen@gmail.com>
2018-09-21 09:50:01 +0200

    Dynamiske og anonyme typer.

---
+++ AnonymeTyper/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MCronberg;
+
+namespace AnonymeTyper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            dynamic hest = new { navn = "Per", alder = "10" };
+
+            Console.WriteLine($"Hesten hedder {hest.navn} og er {hest.alder} år.");
+            SkrivHest(hest);
+
+            dynamic kat = new ExpandoObject();
+            kat.navn = "Pelle";
+            kat.alder = 65;
+
+            Console.WriteLine($"Katten hedder {kat.navn} og er {kat.alder} år.");
+            SkrivHest(kat);
+
+            // Keep console open when debugging
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+
+
+        public static void SkrivHest(dynamic a) => Console.WriteLine($"Hesten hedder {a.navn} og er {a.alder} år.");
+
+    }
+}
